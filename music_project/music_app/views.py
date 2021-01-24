49e1b1ac31f046038032694677004a4ff7db4298
braddock <braddock511@gmail.com>
2021-01-24 23:59:20 +0100

    Added models serializer and user create endpoint

--- music_project/music_app/views.py
+++ music_project/music_app/views.py
@@ -1,53 +1,16 @@
 from rest_framework import generics
-from .models import Artist, Album, Song, Playlist, Playlist_Song
-from .serializers import ArtistSerializer, AlbumSerializer, SongSerializer, PlaylistSerializer, PlaylistSongSerializer
+from .models import Song, Playlist, User, SongPlaylist
+from .serializers import SongSerializer, PlaylistSerializer, UserSerializer
+from rest_framework import permissions
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-create'
+    permission_classes = (permissions.AllowAny, )
 
 
-class ArtistList(generics.ListCreateAPIView):
-    queryset = Artist.objects.all()
-    serializer_class = ArtistSerializer
 
-
-class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Artist.objects.all()
-    serializer_class = ArtistSerializer
-
-class AlbumList(generics.ListCreateAPIView):
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
-
-
-class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
-
-
-class SongList(generics.ListCreateAPIView):
-    queryset = Song.objects.all()
-    serializer_class = SongSerializer
-
-
-class SongDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Song.objects.all()
-    serializer_class = SongSerializer
-    
-class PlaylistList(generics.ListCreateAPIView):
-    queryset = Playlist.objects.all()
-    serializer_class = PlaylistSerializer
-
-
-class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Playlist.objects.all()
-    serializer_class = PlaylistSerializer
-
-class PlaylistSongList(generics.ListCreateAPIView):
-    queryset = Playlist_Song.objects.all()
-    serializer_class = PlaylistSongSerializer
-
-
-class PlaylistSongDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Playlist_Song.objects.all()
-    serializer_class = PlaylistSongSerializer
 
 
 
