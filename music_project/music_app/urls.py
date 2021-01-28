992413f6cae9bd97a4251b6b571db89385b2dbc9
braddock <braddock511@gmail.com>
2021-01-28 11:22:59 +0100

    added api-root, filtering, ordering, permissions, searching to endpoints

--- music_project/music_app/urls.py
+++ music_project/music_app/urls.py
@@ -3,11 +3,13 @@ from music_app import views
 
 
 urlpatterns = [
+    path('accounts', views.UsersList.as_view(), name=views.UsersList.name),
     path('account/register', views.UserCreate.as_view(), name=views.UserCreate.name ),
-    path('users/<int:pk>', views.UsersList.as_view(), name=views.UsersList.name ),
+    path('account/<int:pk>', views.UserDetail.as_view(), name=views.UserDetail.name ),
     path('songs', views.SongList.as_view(), name=views.SongList.name),
     path('songs/<int:pk>', views.SongDetail.as_view(), name=views.SongDetail.name),
     path('playlists', views.PlaylistList.as_view(), name=views.PlaylistList.name),
     path('playlists/<int:pk>', views.PlaylistDetail.as_view(), name=views.PlaylistDetail.name),
     path('playlists-songs', views.SongInThePlaylist.as_view(), name=views.SongInThePlaylist.name),
+    path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
--- music_project/music_app/views.py
+++ music_project/music_app/views.py
@@ -2,43 +2,79 @@ from rest_framework import generics
 from .models import Song, Playlist, User, SongPlaylist
 from .serializers import SongSerializer, PlaylistSerializer, UserSerializer, SongPlaylistSerializer
 from rest_framework import permissions
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
 
-class UsersList(generics.RetrieveAPIView):
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-detail'
+    permission_classes = [permissions.IsAuthenticated]
+    
+class UsersList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     name = 'users-list'
+    search_fields = ['name']
+    ordering_fields = ['name']
+    permission_classes = [permissions.IsAdminUser]
+
+
 
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     name = 'user-create'
-    permission_classes = (permissions.AllowAny, )
+    permission_classes = [permissions.AllowAny]
 
 
 class SongDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     name = 'song-detail'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
 
 class SongList(generics.ListCreateAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     name = 'song-list'
+    filterset_fields = ['artist', 'album']
+    search_fields = ['track']
+    ordering_fields = ['track', 'artist', 'album']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class PlaylistDetail(generics.ListCreateAPIView):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
     name = 'playlist-detail'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class PlaylistList(generics.ListCreateAPIView):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
     name = 'playlist-list'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     
 class SongInThePlaylist(generics.ListCreateAPIView):
     queryset = SongPlaylist.objects.all()
     serializer_class = SongPlaylistSerializer
-    name = 'songs-in-the-playlists'  
+    name = 'songs-in-the-playlists' 
+    filterset_fields = ['id_playlist'] 
+    ordering_fields = ['id_playlist']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ApiRoot(generics.GenericAPIView):
+    name = 'api-root'
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'users-list': reverse(UsersList.name, request=request),
+            'user-create': reverse(UserCreate.name, request=request),
+            'song-list': reverse(SongList.name, request=request),
+            'playlist-list': reverse(PlaylistList.name, request=request),
+            'songs-in-the-playlists': reverse(SongInThePlaylist.name, request=request)
+        })
 
 
 
